f799b89ecb2f0f1a3d8c36f47e12e66f9fda1ff7
Himanshu Parodia <himanshuparodiya@gmail.com>
2023-01-02 15:50:03 +0530

    test with gitignore

---
+++ script.js
@@ -0,0 +1,49 @@
+let myKey = myApi.myApiKey;
+
+let weather = {
+    "apiKey" : myKey,
+    fetchWeather : function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+         + city
+         + "&units=metric&appid="
+         + this.apiKey
+        )
+        .then((response ) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather : function (data) {
+        const {name} = data;
+        const {icon,description} = data.weather[0];
+        const {temp,humidity} = data.main;
+        const {speed} = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = " Humidity "  + humidity + "%";
+        document.querySelector(".wind").innerText = " Wind speed " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading")
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')"
+    },
+    // part of search bar
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+}
+
+// functional search bar
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+})
+
+// search by enter key
+
+document.querySelector(".search-bar").addEventListener("keyup", function () {
+    if (event.key == "Enter"){
+        weather.search()
+    }
+})
+
+// it will run when we'll enter in page first time 
+weather.fetchWeather("Delhi")
