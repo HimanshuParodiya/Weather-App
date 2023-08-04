8a1c46f853e0206441dcfb37526d576deba112d4
Himanshu <himanshuparodiya@gmail.com>
2023-08-04 13:23:41 +0530

    set api key

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-let myKey = myApi.myApiKey;
+
+
 
 let weather = {
-    "apiKey" : myKey,
-    fetchWeather : function (city) {
+    apiKey: "5acc718fea1d17dd1102c8e282b426d1",
+    fetchWeather: function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q="
-         + city
-         + "&units=metric&appid="
-         + this.apiKey
+            + city
+            + "&units=metric&appid="
+            + this.apiKey
         )
-        .then((response ) => response.json())
-        .then((data) => this.displayWeather(data));
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
     },
-    displayWeather : function (data) {
-        const {name} = data;
-        const {icon,description} = data.weather[0];
-        const {temp,humidity} = data.main;
-        const {speed} = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = " Humidity "  + humidity + "%";
+        document.querySelector(".humidity").innerText = " Humidity " + humidity + "%";
         document.querySelector(".wind").innerText = " Wind speed " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading")
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
     // part of search bar
     search: function () {
@@ -40,7 +41,7 @@ document.querySelector(".search button").addEventListener("click", function () {
 // search by enter key
 
 document.querySelector(".search-bar").addEventListener("keyup", function () {
-    if (event.key == "Enter"){
+    if (event.key == "Enter") {
         weather.search()
     }
 })
